2f80ab2ac3f233237fcf65123664e101816df303
Jannatul Ferdous <jannatulferdous8558@gmail.com>
2026-03-10 15:47:27 +0600

    auth fixing

--- cloth_store/settings.py
+++ cloth_store/settings.py
@@ -98,12 +98,17 @@ CLOUDINARY_STORAGE = {
 }
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER 
+SERVER_EMAIL = EMAIL_HOST_USER
 
 # settings.py
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
